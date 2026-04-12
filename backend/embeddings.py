7d781bb48b198abffe794948b306bf8318f43308
shaifali Bhardwaj <bhardwajshaifali87@gmail.com>
2026-04-12 21:39:22 +0530

    Code updated - running

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,15 +1,22 @@
 # from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.embeddings.openai import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
 def create_vector_store(chunks):
     embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
+    # vectorstore=Chroma(
+    # persist_directory="chroma_db",
+    # embedding_function=embedding
+    # )
     vectorstore = Chroma.from_documents(
     documents=chunks,
     embedding=embedding,
-    persist_directory="./chroma_db"
+    persist_directory="chroma_db"
+    # client_settings={"anonymized_telemetry": "False"}
     )
+    # if chunks:
+    #     vectorstore.add_documents(chunks)
+    
 
     vectorstore.persist()
     return vectorstore
--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -1,16 +1,17 @@
 # from langchain.chat_models import ChatOpenAI
 from langchain_community.chat_models import ChatOllama
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import Chroma
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.vectorstores import Chroma
 # from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def load_rag_chain():
     embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
     vectorstore = Chroma(
-    persist_directory="./chroma_db",
+    persist_directory="chroma_db",
     embedding_function=embedding
+    # client_settings={"anonymized_telemetry": "False"}
     )
 
     retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
@@ -20,8 +21,8 @@ def load_rag_chain():
 
     qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
-    retriever=retriever#,
-    # return_source_documents=True
+    retriever=retriever,
+    return_source_documents=True
     )
 
     return qa_chain
@@ -29,9 +30,14 @@ def load_rag_chain():
 
 def ask_question(query):
     qa_chain = load_rag_chain()
-    result = qa_chain(query)
-
-    answer = result["result"]
-    sources = result["source_documents"]
-
+    # result = qa_chain(query)
+    result = qa_chain.invoke({"query": query})
+    if isinstance(result, dict):
+        # answer = result["result"]
+        # sources = result["source_documents"]
+        answer = result.get("result", "No answer found.")
+        sources = result.get("source_documents", [])
+    else:
+        answer = str(result)
+        sources = []
     return answer, sources
